a1771af1a95d4b318698356250f0a6027c2bd96e
Diana Vlasenkova <dianadomino24work@gmail.com>
2020-11-09 17:49:15 +0300

    fix

---
+++ src/components/icons/play/SvgPlayButton.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+function SvgPlayButton(props) {
+    return (
+        <svg
+            width="16px"
+            height="20px"
+            viewBox="0 0 16 20"
+            fill={props.maincolor}
+            {...props}
+        >
+            <path d="M15.258 8.625c.99.651.99 2.099 0 2.75l-12.696 8.35C1.464 20.448 0 19.662 0 18.35V1.65C0 .338 1.464-.447 2.562.275l12.696 8.35z" />
+        </svg>
+    )
+}
+
+export default SvgPlayButton
