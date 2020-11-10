60ee181a5bde041047895eab6bf34f43a626ecce
Mila Sergeeva <buka4a@gmail.com>
2020-11-10 23:56:56 +0100

    Format all project with prettier

--- src/components/Release.js
+++ src/components/Release.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
 
 export default function Release({ release, handleReleaseClick }) {
-    function handleClick() {
-        handleReleaseClick(release)
-    }
-    return (
-        <li className="details__song-item" onClick={handleClick}>
-            <div className="song-item__name-wrap song-item__name-wrap_type_release">
-                {release.title} &mdash; {release.author}
-            </div>
-        </li>
-    )
+  function handleClick() {
+    handleReleaseClick(release)
+  }
+  return (
+    <li className="details__song-item" onClick={handleClick}>
+      <div className="song-item__name-wrap song-item__name-wrap_type_release">
+        {release.title} &mdash; {release.author}
+      </div>
+    </li>
+  )
 }
