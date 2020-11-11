5ebc73c5a6e53dbb3f0d1ffbfa1ecdf59fa0268c
Mila Sergeeva <buka4a@gmail.com>
2020-11-11 17:50:07 +0100

    api adj

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,31 @@
+class Api {
+  constructor(options) {
+    this._url = options.url
+    this._headers = options.headers
+  }
+
+  putInfo() {
+    return fetch(`${this._url}`, {
+      method: 'PUT',
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json()
+      }
+      // если ошибка, отклоняем промис
+      Promise.reject(new Error(`Ошибка: ${res.status}`))
+    })
+  }
+}
+
+const api = new Api({
+  url: 'https://xxx',
+  headers: {
+    authorization: 'xxx',
+    'Content-Type': 'application/json',
+  },
+})
+
 // получение объекта с песнями от сервера
 
 // function getSongsList() {
@@ -45,3 +73,5 @@
 //   formSubmit()
 //   reset()
 // }
+
+export default api
